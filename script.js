c301e068b4288f8b0c7872a3f030c12c3255b93d
Hrishi <shubhamkarale768ms@gmail.com>
2023-06-23 14:10:55 +0530

    Added Files

---
+++ script.js
@@ -0,0 +1,16 @@
+// Addiing Buttons Styles
+// 1. Add a class to the button element
+// 2. Add a class to the button element
+// 3. Add a class to the button element
+// 4. Add a class to the button element
+
+
+
+// Path: script.js
+// Adding a class to the button element
+
+ let a = 5;
+ let b = 10;
+    let c = a + b;
+    console.log(c);
+    
